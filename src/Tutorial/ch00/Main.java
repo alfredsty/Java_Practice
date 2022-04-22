ca80bad6f297d42dfb23a18f31a2d4f67ad98ae8
KuroS2 <102028778+alfredsty@users.noreply.github.com>
2022-04-22 23:59:58 +0900

    commit

--- src/Tutorial/ch00/Main.java
+++ src/Tutorial/ch00/Main.java
@@ -3,6 +3,6 @@ package Tutorial.ch00;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("hello world"    );
+        System.out.println("Hello World!");
     }
 }
